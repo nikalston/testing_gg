5a4092d9a7129e0d571fb51195b512a3f6777f87
nikalston <nik@nikalston.com>
2024-02-24 10:51:11 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
 def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAF6BAKJKR45SAWSZ5", "hjsf70iex5u34565AWS654/JKGjhz545d89sjkja")
+    database = aws_lib.connect("AKIAF6BAKJKR45WAWSZ5", "hjsf70iex5u24565AWS654/JKGjhz545d89sjkja")
     database.push(data)
   
